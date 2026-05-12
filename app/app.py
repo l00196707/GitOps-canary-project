2dd75b744b03f3fc07250536a96e08ec44f671da
L00196707 <l00196707@atu.ie>
2026-05-12 12:41:27 +0100

    fix lint errors

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+"""import os for env variables"""
 import os
+from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -7,7 +9,11 @@ VERSION = os.getenv("APP_VERSION", "Version 1")
 
 @app.route("/")
 def home():
+    """
+    home page
+    """
     return f"Hello from {VERSION}"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
